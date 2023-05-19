5c269a3fa7ba6720e3b04bc72d61817b04984666
Hafurfur <rogmesec@yandex.ru>
2023-05-19 17:42:02 +0300

    add logging, path selection flag

--- src/game_procestion.py
+++ src/game_procestion.py
@@ -1,19 +1,23 @@
+import logging
 import pathlib
+import os
 import subprocess
-
-from settings import GAME_LOCATION_EXE, GAME_PROCESS
+from settings import STEAM_FLAG, GAME_LOCATION_EXE
 
 
 class GameProc:
     def __init__(self):
-        self.subproc = None
+        self.game_loc: str = os.environ['GAME_LOCATION_EXE'] if STEAM_FLAG == 'Y' else GAME_LOCATION_EXE
 
-    def game_star(self):
-        game_path = pathlib.Path(GAME_LOCATION_EXE)
-        subprocess.Popen([game_path, 'start'])
-        print(f'Game load')
+    def star_game(self):
+        game_path = pathlib.Path(self.game_loc, 'BetonBrutal.exe')
+        try:
+            subprocess.Popen([game_path, 'start'])
+        except Exception:
+            logging.exception('Failed to start the game')
+            raise
+        print(f'--> Game load')
 
 
 if __name__ == '__main__':
-    gg = GameProc()
-    # gg.game_star()
+    pass
